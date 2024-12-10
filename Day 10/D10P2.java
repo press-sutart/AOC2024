166d043fda9b7651cd1a0fdd615a8f1ab4f7e0ac
press-sutart <61983502+press-sutart@users.noreply.github.com>
2024-12-10 18:59:43 +0800

    add day 10

---
+++ Day 10/D10P2.java
@@ -0,0 +1,115 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class D10P2 {
+    public record Coords(int r, int c) {
+        public Coords add(Coords rhs) {
+            return new Coords(r + rhs.r, c + rhs.c);
+        }
+    }
+
+    public static class MemoReachableNines {
+        private ArrayList<ArrayList<Integer>> grid;
+        private HashMap<Coords, Integer> memoResults;
+        private int rows;
+        private int cols;
+        private final Coords[] DC = {
+            new Coords(1, 0), new Coords(0, 1),
+            new Coords(-1, 0), new Coords(0, -1)
+        };
+
+        public Integer readGrid(Coords co) {
+            return grid.get(co.r()).get(co.c());
+        }
+
+        public Boolean inBounds(Coords co) {
+            return 0 <= co.r() && co.r() < rows &&
+                   0 <= co.c() && co.c() < cols;
+        }
+
+        public Integer getReachableNines(Coords co) {
+            if (memoResults.containsKey(co)) {
+                return memoResults.get(co);
+            }
+
+            if (!inBounds(co)) {
+                return 0;
+            }
+
+            if (readGrid(co) == 9) {
+                return 1;
+            }
+
+            Integer temp = 0;
+
+            for (int i = 0; i < 4; i++) {
+                Coords nextco = co.add(DC[i]);
+                if (inBounds(nextco) && readGrid(co) + 1 == readGrid(nextco)) {
+                    temp += getReachableNines(nextco);
+                }
+            }
+
+            memoResults.put(co, temp);
+            return temp;
+        }
+
+        public MemoReachableNines(ArrayList<ArrayList<Integer>> grid) {
+            memoResults = new HashMap<>();
+            this.grid = grid;
+            rows = grid.size();
+            cols = (rows == 0) ? 0 : grid.get(0).size();
+        }
+    }
+
+    public static ArrayList<ArrayList<Integer>> input(Scanner scanner) {
+        ArrayList<ArrayList<Integer>> inputGrid = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String inputLine = scanner.nextLine();
+            ArrayList<Integer> lineNumbers = new ArrayList<>();
+            for (char inputChar : inputLine.toCharArray()) {
+                lineNumbers.add(inputChar - '0');
+            }
+            inputGrid.add(lineNumbers);
+        }
+        return inputGrid;
+    }
+
+    public static ArrayList<ArrayList<Integer>> readFromFile(String filePath) {
+        try {
+            File inputFile = new File(filePath);
+            Scanner scanner = new Scanner(inputFile);
+            ArrayList<ArrayList<Integer>> inputGrid = input(scanner);
+            scanner.close();
+            return inputGrid;
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open file");
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+    
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> inputGrid = readFromFile(
+            "Day 10/input.txt"
+        );
+
+        int rows = inputGrid.size();
+        int cols = (rows == 0) ? 0 : inputGrid.get(0).size();
+        MemoReachableNines fun = new MemoReachableNines(inputGrid);
+        int total = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (inputGrid.get(r).get(c) == 0) {
+                    Coords co = new Coords(r, c);
+                    total += fun.getReachableNines(co);
+                }
+            }
+        }
+
+        System.out.println(total);
+    }
+}
---
+++ Day 10/README.md
@@ -0,0 +1,23 @@
+# Day 10
+
+Back to Java.
+
+Movement in the grid is one-way, in the sense that we are on a position with height $`i`$, we can only move to neighbouring positions with height $`i+1`$. This allows a dynamic programming approach for both parts, where the state parameters are positions on the grid and transitions are movements. The base states are the positions with height $`9`$. Since the states and transitions of both parts are almost identical, my codes for both parts are similar as well.
+
+I did dynamic programming by defining a class. A method of the class describes the DP recursion, while a `HashMap` memoises previously computed results. 
+
+## Part 1
+
+The question asks how many $9$'s are reachable from each $0$. A variant of the task that is solvable using DP is as follows: what is the set of $9$'s that are reachable from each position? The base case is easy: for any $9$, the only reachable $9$ is itself. As for the transition, suppose the current position has height $i < 9$. Then the set of $9$'s reachable from the current position is the union of sets of $9$'s reachable from the neighbouring positions we can move to of height $i+1$.
+
+To solve the original problem, I called the DP function on each location of a $0$ and summed up the sizes of the sets.
+
+Let the size of the grid be $`n \times n`$ and the maximum height be $h$ (here $h = 9$). Since the graph is grid-shaped, the maximum number of maximum height positions reachable from any position is proportional to $h$. There are $`O(n^2)`$ states, a transition needs $`O(h)`$ time and memoising a single result needs $`O(h)`$ space. Hence, the solution takes $`O(n^2 h)`$ time and space.
+
+## Part 2
+
+The question asks how many paths to $9$'s there are from each $0$. A variant of the task that is solvable using DP is as follows: how many paths to $9$'s are there from each position? Any $9$ has only one path to a $9$ (not moving at all), while the number of paths to $9$'s from any other position is the sum of numbers of paths to $9$'s from its neighbouring positions of height $i+1$.
+
+I called the DP function on each location of a $0$ and summed up the results.
+
+There are $`O(n^2)`$ states, a transition needs constant time and memoising a single result needs constant space. Hence, the solution takes $`O(n^2)`$ time and space.
